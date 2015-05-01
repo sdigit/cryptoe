72421f12dc9dd4b33b22003e46c45939ce55d7de
sdigit <gitmoney@endersgame.net>
2015-05-01 15:25:12 -0500

    Add REFS and CREDITS Make README less barebones Add docstrings

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-cd cryptoe && python setup.py install --user
+## CRYPTOE: ##
+#### A project aiming to offer a full set of cryptographic routines to make implementing crypto in any Python project easy. ####
 
-I wouldn't use this system-wide yet, as it's still missing functionality.
+To install:
+> cd cryptoe && python setup.py install --user
+(I don't recommend installing this system-wide yet, as it's still WIP while I merge code out of other projects and clean
+it up)
 
-Note: this WILL NOT work on a system lacking Intel RDRAND ("Bull Mountain") instructions!
+### CAVEATS ###
+Due to heavy reliance on Intel RDRAND, this WILL NOT work on a system lacking it.
+
+### TODO ###
+1. Make the RNG configurable. I want to limit the choices, at least at first, so that I can test each (rather than simply having it take anything with a getrandbits function)
+2. Add unit tests, primarily selftests for the algorithms
+3. Add RNG test results. Right now I have a lot of data from dieharder on RDRAND, as that's why Cryptoe uses it, but for the sake of completeness it should include dieharder tests of all supported RNGs.
+4. Add a generic KMS class for storage of key data and key metadata in a secure manner. This will most likely be done first for sqlite3 and psycopg2.
+
+
 
--- cryptoe/cryptoe/utils.py
+++ cryptoe/cryptoe/utils.py
@@ -1,7 +1,20 @@
+"""
+Centralize all cryptography primitives (as well as data conversion routines) that are commonly needed, in order to
+ensure that implementations differ as little as possible.
+
+Where non-cryptoe code is used (eg. pycrypto), I have endeavored to verify that the implementation being referenced is
+compliant with the relevant standards (NIST or RFC), however can only vouch for the versions audited at the time this
+was written.
+
+For a complete list of which documents the cryptoe package implements in whole or in part, see the REFS file in the
+master cryptoe directory.
+"""
+
 __author__ = 'Sean Davis <dive@endersgame.net>'
 
-from Crypto.Util import Counter
-from Crypto.Hash import HMAC, SHA512
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Util import Counter, RFC1751
+from Crypto.Hash import HMAC, SHA256
 from Crypto.Cipher import AES
 import math
 import struct
@@ -9,6 +22,7 @@ import rdrand
 
 DEFAULT_PBKDF2_ITERATIONS = 20000
 QUAD = struct.Struct('>Q')
+
 
 def long2ba(val):
     """
@@ -41,6 +55,7 @@ def ba2long(val):
             N += ord(b)
     return long(N)
 
+
 def rndbytes(sz):
     """
     Return sz random bytes
@@ -55,7 +70,14 @@ def __pack_for_kdf(string):
 
 
 def __prf(k, s):
-    h = HMAC.new(k, digestmod=SHA512)
+    """
+    This provides the Pseudo-Random Function for CTR-KDF.
+
+    :param k: Key
+    :param s: Data
+    :return:
+    """
+    h = HMAC.new(k, digestmod=SHA256)
     h.update(s)
     r = h.digest()
     del h
@@ -87,6 +109,13 @@ def ctrkdf(key, label, context, sz=256):
 
 
 def ctr_enc(k, msg):
+    """
+    AES256 in CTR mode (Encrypt)
+
+    :param k: Key
+    :param msg: Plaintext
+    :return: Ciphertext
+    """
     ctr = Counter.new(128)
     aes = AES.new(k, AES.MODE_CTR, counter=ctr)
     ct = aes.encrypt(msg)
@@ -96,6 +125,13 @@ def ctr_enc(k, msg):
 
 
 def ctr_dec(k, msg):
+    """
+    AES256-CTR (Decrypt)
+
+    :param k: Key
+    :param msg: Ciphertext
+    :return: Plaintext
+    """
     ctr = Counter.new(128)
     aes = AES.new(k, AES.MODE_CTR, counter=ctr)
     ct = aes.decrypt(msg)
@@ -154,3 +190,47 @@ def aes_wrap_key_withpad(kek, plaintext):
     plaintext += "\0" * (8 - len(plaintext) % 8)
     return aes_wrap_key(kek, plaintext, iv)
 
+def key128_to_str(key):
+    """
+    Convert 128-bit key (bytes or int) to RFC1751 format
+    :param key: bytes or int containing 128 bits to be converted to words
+    :rtype: str
+    :return: RFC1751 words
+    """
+    if isinstance(key, int):
+        return RFC1751.key_to_english(bytes(key))
+    elif isinstance(key, bytes):
+        return RFC1751.key_to_english(key)
+    else:
+        raise TypeError('expected int or bytes')
+
+
+def str_to_key128(words):
+    """
+    Convert RFC1751 encoded key to bytes
+    :param words: the words
+    :type words: str
+    :return: bytes
+    :rtype: bytes
+    """
+    if not isinstance(words, str):
+        raise TypeError('expected string')
+    return RFC1751.english_to_key(words)
+
+
+def pw2key(pw, salt=None, digest=SHA256):
+    """
+    This function is a wrapper for purposes of convenience around Crypto.Protocol.KDF.PBKDF2
+    PBKDF2 per NIST SP800-132 is used with HMAC-SHA256 as a default. Salt and an alternate digest can be specified.
+    A list containing the derived key and the salt is returned.
+
+    :param pw: password
+    :param salt: salt for PBKDF
+    :param digest:
+    :return: {'key': key, 'salt': salt}
+    :rtype: dict
+    """
+    lprf = lambda x, y: HMAC.new(x, msg=y, digestmod=digest).digest()
+    if not salt:
+        salt = rndbytes(32)
+    return [PBKDF2(pw, salt, dkLen=32, prf=lprf, count=DEFAULT_PBKDF2_ITERATIONS), salt]
