e45a32505dec39cf980850023c237324f70ceadf
sdigit <gitmoney@endersgame.net>
2015-05-18 22:47:21 -0500

    restructure and add ciphers

--- cryptoe/setup.py
+++ cryptoe/setup.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python2
 from distutils.core import setup, Extension
+import os
 
 cryptoe_ext = Extension('cryptoe_ext',
-                        sources=['src/rdrand.c',
-                                 'src/sha2.c',
-                                 'src/hmac_sha2.c',
+                        include_dirs=[os.path.join(os.getcwd(),'src','include')],
+                        sources=['src/rng/rdrand.c',
+                                 'src/hash/sha2.c',
+                                 'src/mac/hmac_sha2.c',
+                                 'src/cipher/twofish.c',
+                                 'src/cipher/serpent.c',
                                  'src/cryptoe.c'])
 
 setup(name='cryptoe',
       author='Sean Davis',
       author_email='cryptoe@endersgame.net',
-      version='1.5.0-dev',
+      version='1.5.1-wip',
       url='https://github.com/sdigit/cryptoe/',
       description='Small, easily integrated library for simple cryptography applications, avoiding OpenSSL.',
       packages=['cryptoe'],
