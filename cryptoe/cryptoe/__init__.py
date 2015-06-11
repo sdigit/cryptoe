2b12866eaf03fbaedc5ef033cf5314e16abecc43
sdigit <gitmoney@endersgame.net>
2015-06-10 22:28:00 -0500

    bump since lots is getting refactored

--- cryptoe/cryptoe/__init__.py
+++ cryptoe/cryptoe/__init__.py
@@ -2,8 +2,8 @@ import struct
 
 __author__ = 'Sean Davis <dive@endersgame.net>'
 __all__ = ['utils']
-__version__ = '1.5.3a0'
-version_info = (1, 5, 3, 'alpha', 0)
+__version__ = '1.6.0'
+version_info = (1, 6, 0)
 
 DEFAULT_PBKDF2_ITERATIONS = 20000
 QUAD = struct.Struct('>Q')
