807a08cfe292a8d16e31f2a117aef9c30014d5e2
sdigit <gitmoney@endersgame.net>
2015-06-11 05:18:24 -0500

    move things where they belong

--- cryptoe/cryptoe/utils.py
+++ cryptoe/cryptoe/utils.py
@@ -17,7 +17,23 @@ import struct
 from Crypto.Util import Counter
 from Crypto.Cipher import AES
 
-from cryptoe import Random, QUAD
+from cryptoe import Random
+
+
+def pad(what, size):
+    if len(what) > size:
+        raise ValueError('length exceeds desired padded length')
+    elif len(what) == size:
+        return what
+    wlen = len(what)
+    plen = size - wlen
+    out = struct.pack('<s', what)
+    pad_bytes = [struct.pack('B', _) for _ in xrange(0, plen)]
+    out += ''.join(pad_bytes)
+    del pad_bytes
+    del wlen
+    del plen
+    return out
 
 
 def long2ba(val):
@@ -92,58 +108,6 @@ def ctr_dec(k, msg):
     del ctr
     del aes
     return ct
-
-
-# noinspection PyTypeChecker
-def aes_unwrap_key_and_iv(kek, wrapped):
-    n = len(wrapped) / 8 - 1
-    R = [None] + [wrapped[i * 8:i * 8 + 8] for i in range(1, n + 1)]
-    A = QUAD.unpack(wrapped[:8])[0]
-    decrypt = AES.new(kek).decrypt
-    for j in range(5, -1, -1):
-        for i in range(n, 0, -1):
-            ciphertext = QUAD.pack(A ^ (n * j + i)) + R[i]
-            B = decrypt(ciphertext)
-            A = QUAD.unpack(B[:8])[0]
-            R[i] = B[8:]
-    return "".join(R[1:]), A
-
-
-def aes_unwrap_key(kek, wrapped, iv=0xa6a6a6a6a6a6a6a6):
-    key, key_iv = aes_unwrap_key_and_iv(kek, wrapped)
-    if key_iv != iv:
-        raise ValueError("Integrity Check Failed: " + hex(key_iv) + " (expected " + hex(iv) + ")")
-    return key
-
-
-def aes_unwrap_key_withpad(kek, wrapped):
-    key, key_iv = aes_unwrap_key_and_iv(kek, wrapped)
-    key_iv = "{0:016X}".format(key_iv)
-    if key_iv[:8] != "A65959A6":
-        raise ValueError("Integrity Check Failed: " + key_iv[:8] + " (expected A65959A6)")
-    key_len = int(key_iv[8:], 16)
-    return key[:key_len]
-
-
-def aes_wrap_key(kek, plaintext, iv=0xa6a6a6a6a6a6a6a6):
-    n = len(plaintext) / 8
-    R = [None] + [plaintext[i * 8:i * 8 + 8] for i in range(0, n)]
-    A = iv
-    encrypt = AES.new(kek).encrypt
-    for j in range(6):
-        for i in range(1, n + 1):
-            # noinspection PyTypeChecker
-            B = encrypt(QUAD.pack(A) + R[i])
-            A = QUAD.unpack(B[:8])[0] ^ (n * j + i)
-            R[i] = B[8:]
-    # noinspection PyTypeChecker
-    return QUAD.pack(A) + "".join(R[1:])
-
-
-def aes_wrap_key_withpad(kek, plaintext):
-    iv = 0xA65959A600000000 + len(plaintext)
-    plaintext += "\0" * (8 - len(plaintext) % 8)
-    return aes_wrap_key(kek, plaintext, iv)
 
 
 _rng = Random.new()
