47956dac2c4a40457d4722ffd98a51617dc5f3fb
sdigit <gitmoney@endersgame.net>
2015-06-15 20:13:34 -0500

    Add more exceptions.

--- cryptoe/cryptoe/exceptions.py
+++ cryptoe/cryptoe/exceptions.py
@@ -16,3 +16,23 @@ class ImmutableError(CryptoKeyError):
 
 class DerivationError(CryptoKeyError):
     """A derived key did not pass checks"""
+
+
+class KeyLengthError(CryptoKeyError):
+    """Returned key is not of expected length"""
+
+
+class SaltError(CryptoError):
+    """Main class for Salt-related errors"""
+
+
+class SaltLengthError(SaltError):
+    """Salt is not of expected length"""
+
+
+class SafetyMargin(Exception):
+    """Class for exceptions raised when things are not actually errors, just not acceptably safe."""
+
+
+class LowIterationCount(SafetyMargin):
+    """CPU is cheap, security is not"""
