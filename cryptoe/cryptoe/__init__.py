65e1f3f5074342c765eddab3a1adaadae9d27223
sdigit <gitmoney@endersgame.net>
2015-06-15 20:17:58 -0500

    add the beginning of a command like utility; update credits (more entries are needed there, im missing people) bump to 1.9.0

--- cryptoe/cryptoe/__init__.py
+++ cryptoe/cryptoe/__init__.py
@@ -2,8 +2,8 @@ import struct
 
 __author__ = 'Sean Davis <dive@endersgame.net>'
 __all__ = ['utils', 'KeyMgmt', 'KeyWrap', 'KeyDB', 'Hash']
-__version__ = '1.7.1'
-version_info = (1, 7, 1)
-
-DEFAULT_PBKDF2_ITERATIONS = 20000
+version_info = (1, 9, 0)
+__version__ = '.'.join([str(_) for _ in version_info])
+DEFAULT_PBKDF2_ITERATIONS = 2 ** 16
+MINIMUM_PBKDF2_ITERATIONS = 2 ** 15
 QUAD = struct.Struct('>Q')
--- cryptoe/setup.py
+++ cryptoe/setup.py
@@ -2,33 +2,38 @@
 import os
 from distutils.core import setup, Extension
 
-# Add these once a context object is ready:
-# src/cipher/twofish.c
-# src/cipher/serpent.c
-
 cryptoe_ext = Extension('cryptoe_ext',
                         include_dirs=[os.path.join(os.getcwd(), 'src', 'include')],
                         sources=['src/rng/rdrand.c',
                                  'src/cryptoe.c'])
-
+shad256_ext = Extension('cryptoe.Hash.SHAd256',
+                        include_dirs=[os.path.join(os.getcwd(), 'src', 'include')],
+                        sources=['src/hash/SHAd256.c'])
 setup(
     name='cryptoe',
     author='Sean Davis',
     author_email='cryptoe@endersgame.net',
-    version='1.7.1',   # see also cryptoe/__init__.py
+    version='1.9.0',   # see also cryptoe/__init__.py
     url='https://github.com/sdigit/cryptoe/',
     description='Small, easily integrated library for simple cryptography applications, avoiding OpenSSL.',
     packages=[
         'cryptoe',
+        'cryptoe.Hash',
         'cryptoe.Random',
     ],
     py_modules=[
+        'cryptoe.exceptions',
         'cryptoe.utils',
+        'cryptoe.KeyMgmt',
+        'cryptoe.KeyDB',
+        'cryptoe.KeyWrap',
     ],
     ext_modules=[
-        cryptoe_ext
+        cryptoe_ext,
+        shad256_ext,
     ],
     requires=[
-        'Crypto', 'hkdf', 'sqlalchemy',
-    ]
+        'Crypto', 'hkdf', 'sqlalchemy', 'whirlpool',
+    ],
+    scripts=['kdb'],
 )
