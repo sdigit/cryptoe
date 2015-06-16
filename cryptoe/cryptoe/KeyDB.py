1cdce76615f5d3526963d984894f6657b61947f7
sdigit <gitmoney@endersgame.net>
2015-06-15 20:15:51 -0500

    clean, improve, probably added bugs too. needs a lot of testing but works.

--- cryptoe/cryptoe/KeyDB.py
+++ cryptoe/cryptoe/KeyDB.py
@@ -1,14 +1,15 @@
-import base64
-
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy import create_engine, Column, Integer, String, Binary, DateTime, ForeignKey, \
     func
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import NoResultFound
 
 from KeyWrap import KW
 from cryptoe import Random
 from cryptoe.KeyMgmt import newkey_pbkdf, newkey_hkdf, pack_hkdf_info, newkey_rnd, DEFAULT_PRF_HASH, SHAd256_HEX
+from cryptoe.exceptions import KeyLengthError, SaltLengthError
+from utils import yubikey_passphrase_cr
 
 KEYDB_USER = 'KeyDB'
 KEYDB_PURPOSE_MASTER = 'KDB Master'
@@ -16,7 +17,10 @@ KEYDB_PURPOSE_WRAPPING = 'KDB Wrapping'
 KEYDB_PURPOSE_DERIVATION = 'KDF'
 KEYDB_PURPOSE_ROOT_ENC = 'Cipher Root Key'
 KEYDB_PURPOSE_ROOT_MAC = 'HMAC Root Key'
+
 KEYDB_PBKDF2_ITERATIONS = 2 ** 20
+KEYDB_PASSPHRASE_LENGTH = 20
+KEYDB_YUBIKEY_CR_SLOT = 2
 
 Base = declarative_base()
 
@@ -60,12 +64,17 @@ class Key(Base):
 def new_random_key(maker, kek, purpose, user, klen=32):
     session = maker()
     no_salt = session.query(Salt).filter_by(salt='').first()
+    if len(no_salt.salt) != 0:
+        raise SaltLengthError('Salt length for new_random_key should be 0')
     k = Key()
     k.bits = klen * 8
     k.purpose = purpose
     k.user = user
     k.salt_id = no_salt.id
     k_actual = newkey_rnd(klen)
+    if len(k_actual) != klen:
+        raise KeyLengthError('Key returned by newkey_rnd does not match requested length (%d != %d)' % (len(k_actual),
+                                                                                                        klen))
     k.key = KW.wrap(kek, k_actual)
     k_hash = SHAd256_HEX(k_actual)
     k.hash_shad256 = k_hash
@@ -82,9 +91,14 @@ def new_derived_key(maker, kek, kdk, purpose, user, klen=32):
     k.bits = klen * 8
     k.purpose = purpose
     k_salt = new_salt(session, klen)
+    if len(k_salt.salt) != klen:
+        raise SaltLengthError('HKDF salt must be the same length as the derivation key')
     k.salt_id = k_salt.id
     k.user = user
     k_actual = newkey_hkdf(klen, kdk, k_salt.salt, pack_hkdf_info(k.purpose, k.user))
+    if len(k_actual) != klen:
+        raise KeyLengthError('HKDF returned a key with an incorrect length (%d != %d)' % (len(k_actual),
+                                                                                          klen))
     if kek == '':
         k.key = ''
     else:
@@ -115,12 +129,11 @@ def init_keys(maker):
     from getpass import getpass
 
     passphrase_ready = 0
-    passphrase = ''
     session = maker()
+    passphrase = ''
     while not passphrase_ready:
         passphrase = getpass('Master Passphrase: ').rstrip()
-        #        if len(passphrase) < 20:
-        if len(passphrase) < 5:
+        if len(passphrase) < KEYDB_PASSPHRASE_LENGTH:
             print('Passphrase is too short.')
             continue
         else:
@@ -128,9 +141,8 @@ def init_keys(maker):
             if confirm == passphrase:
                 passphrase_ready = 1
     del passphrase_ready
-
     roundcount = KEYDB_PBKDF2_ITERATIONS
-
+    passphrase = yubikey_passphrase_cr(passphrase)
     klen_bits = 256
     klen = klen_bits / 8
 
@@ -144,6 +156,8 @@ def init_keys(maker):
     print('Using PBKDF2 with %d rounds of %s' % (roundcount, mk.prf_hash))
 
     mk_key = newkey_pbkdf(klen, passphrase, mk_salt.salt, roundcount)
+    if len(mk_key) != klen:
+        raise KeyLengthError('PBKDF key is not of requested length (%d != %d)' % (len(mk_key), klen))
     mk_hash = SHAd256_HEX(mk_key)
     mk.hash_shad256 = mk_hash
     session.add(mk)
@@ -157,14 +171,21 @@ def init_keys(maker):
     print('Using master key to derive database key wrapping key')
 
     dk, dk_hash = new_derived_key(maker, kek='', kdk=mk_key, purpose=KEYDB_PURPOSE_WRAPPING, user=KEYDB_USER, klen=32)
+    if len(dk) != klen:
+        raise KeyLengthError('PBKDF key is not of requested length')
     mk_key = '\x00' * klen
     del mk_key
 
     print('Generating random database root key')
 
-    new_random_key(maker, kek=dk, purpose=KEYDB_PURPOSE_MASTER, user=KEYDB_USER, klen=32)
+    dbk, dbk_hash = new_random_key(maker, kek=dk, purpose=KEYDB_PURPOSE_MASTER, user=KEYDB_USER, klen=32)
+    if len(dbk) != klen:
+        raise KeyLengthError('Random key is not of requested length')
+
     dk = '\x00' * klen
     del dk
+    dbk = '\x00' * klen
+    del dbk
 
 
 def db_ready(maker):
@@ -172,6 +193,9 @@ def db_ready(maker):
     try:
         dbk = session.query(Key).filter_by(user=KEYDB_USER,
                                            purpose=KEYDB_PURPOSE_MASTER).one()  # .order_by(desc(Key.created))
+    except NoResultFound:
+        session.close()
+        return False
     except OperationalError:
         session.close()
         return False
@@ -197,6 +221,9 @@ def initialize_db(dbu):
     objs = []
     Base.metadata.create_all(engine)
     sm = sessionmaker(bind=engine)
+    if db_ready(sm) is True:
+        print('Database already initialized!')
+        return None
     session = sm()
     for o in objs:
         session.add(o)
@@ -219,11 +246,12 @@ def get_db_key(maker):
 
     session = maker()
 
-    dbk = session.query(Key).filter_by(user=KEYDB_USER, purpose=KEYDB_PURPOSE_MASTER).one()
+    dbk = session.query(Key).filter_by(user=KEYDB_USER, purpose=KEYDB_PURPOSE_MASTER).order_by(Key.created.desc()).one()
     wk = session.query(Key).filter_by(hash_shad256=dbk.related_hash).one()
     mk = session.query(MasterKey).filter_by(hash_shad256=wk.related_hash).one()
 
     pw = getpass('passphrase: ').rstrip()
+    pw = yubikey_passphrase_cr(pw)
     mk_key = newkey_pbkdf(32, pw, mk.salt.salt, mk.rounds)
     mk_key_hash = SHAd256_HEX(mk_key)
     if mk_key_hash != mk.hash_shad256:
@@ -242,20 +270,5 @@ def get_db_key(maker):
         return None
     else:
         print('database key hash verified')
+    session.close()
     return [dbk_key, dbk_key_hash]
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) != 2:
-        print('Usage: %s <database url (sqlalchemy format)>' % sys.argv[0])
-        sys.exit(0)
-    db_url = sys.argv[1]
-    smaker = open_db(db_url)
-    if not db_ready(smaker):
-        smaker = initialize_db(db_url)
-        init_keys(smaker)
-    else:
-        smaker = open_db(db_url)
-        dbk_inf = get_db_key(smaker)
-        print('dbk = %s' % base64.b64encode(dbk_inf[0]))
