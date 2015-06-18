ed3333527547db60c70f237b7dbf434e8ecb3fec
sdigit <gitmoney@endersgame.net>
2015-06-18 01:48:51 -0500

    bump

--- cryptoe/cryptoe/__init__.py
+++ cryptoe/cryptoe/__init__.py
@@ -2,7 +2,7 @@ import struct
 
 __author__ = 'Sean Davis <dive@endersgame.net>'
 __all__ = ['Random', 'utils', 'KeyMgmt', 'KeyWrap', 'KeyDB', 'Hash']
-version_info = (1, 12, 0)
+version_info = (1, 12, 1)
 __version__ = '.'.join([str(_) for _ in version_info])
 DEFAULT_PBKDF2_ITERATIONS = 2 ** 16
 MINIMUM_PBKDF2_ITERATIONS = 2 ** 15
