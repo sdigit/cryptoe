feff0d1ac15c2b48e225d6457c8616d8b14df2c4
sdigit <gitmoney@endersgame.net>
2015-07-08 10:07:11 -0500

    framework for os-specific stuff

--- cryptoe/cryptoe/OS/__init__.py
+++ cryptoe/cryptoe/OS/__init__.py
@@ -1,3 +1,11 @@
+from os import uname
+
 __author__ = 'Sean Davis <dive@endersgame.net>'
-__all__ = ['LNXKeyring']
+__all__ = ['LNXKeyring', 'Specific']
 from cryptoe.OS import LNXKeyring
+from cryptoe.OS import Specific
+
+_os_name = uname()[0]
+
+function_map = getattr(Specific, '_FUNCTIONS')[_os_name]
+get_sys_uptime = getattr(Specific, function_map['uptime'])
