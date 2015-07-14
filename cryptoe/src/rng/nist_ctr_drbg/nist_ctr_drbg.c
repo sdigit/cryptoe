01aa19f765c5785ac821ea83556cd43795a1ffde
sdigit <gitmoney@endersgame.net>
2015-07-14 03:10:44 -0500

    add license

--- cryptoe/src/rng/nist_ctr_drbg/nist_ctr_drbg.c
+++ cryptoe/src/rng/nist_ctr_drbg/nist_ctr_drbg.c
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2015 Sean Davis <dive@endersgame.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS `AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 /*	$NetBSD: nist_ctr_drbg.c,v 1.1 2011/11/19 22:51:22 tls Exp $ */
 
 /*-
@@ -346,7 +372,7 @@ nist_ctr_drbg_block_cipher_df(const char *input_string[], unsigned int L[],
 static int
 nist_ctr_drbg_block_cipher_df_initialize(void)
 {
-	int i, err;
+	int i,err;
 	unsigned char K[NIST_BLOCK_KEYLEN_BYTES];
 	unsigned int IV[NIST_BLOCK_OUTLEN_INTS];
 
